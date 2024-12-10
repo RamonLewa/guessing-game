5a6b90fbe44902b1167d0339f40acd8fa2b71253
RamonLewa <ramonloewenstein@gmail.com>
2024-12-09 22:07:44 -0300

    Project files

---
+++ Forms/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace guessing_game
+{
+    partial class FGuessingGame
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl1 = new System.Windows.Forms.Panel();
+            this.lblWinner = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pnl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl1
+            // 
+            this.pnl1.BackColor = System.Drawing.Color.White;
+            this.pnl1.Controls.Add(this.lblWinner);
+            this.pnl1.Controls.Add(this.btnReset);
+            this.pnl1.Controls.Add(this.lblResult);
+            this.pnl1.Controls.Add(this.btnGuess);
+            this.pnl1.Controls.Add(this.txtGuess);
+            this.pnl1.Controls.Add(this.lblTitle);
+            this.pnl1.Location = new System.Drawing.Point(12, 12);
+            this.pnl1.Name = "pnl1";
+            this.pnl1.Size = new System.Drawing.Size(563, 320);
+            this.pnl1.TabIndex = 0;
+            // 
+            // lblWinner
+            // 
+            this.lblWinner.AutoSize = true;
+            this.lblWinner.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWinner.Location = new System.Drawing.Point(32, 248);
+            this.lblWinner.Name = "lblWinner";
+            this.lblWinner.Size = new System.Drawing.Size(384, 24);
+            this.lblWinner.TabIndex = 5;
+            this.lblWinner.Text = "Congratulations! You guessed the number in";
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.LightCoral;
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.Location = new System.Drawing.Point(485, 294);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(101, 217);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(206, 24);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "Nice try! The number is";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnGuess.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuess.Location = new System.Drawing.Point(168, 156);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(226, 47);
+            this.btnGuess.TabIndex = 2;
+            this.btnGuess.Text = "Guess!";
+            this.btnGuess.UseVisualStyleBackColor = false;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGuess.Location = new System.Drawing.Point(186, 83);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.ShortcutsEnabled = false;
+            this.txtGuess.Size = new System.Drawing.Size(190, 47);
+            this.txtGuess.TabIndex = 1;
+            this.txtGuess.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtGuess.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtGuess_KeyPress);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(53, 32);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(457, 31);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Guess a number between 1 and 100:";
+            // 
+            // FGuessingGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(587, 344);
+            this.Controls.Add(this.pnl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FGuessingGame";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Guessing game";
+            this.Load += new System.EventHandler(this.FGuessingGame_Load);
+            this.pnl1.ResumeLayout(false);
+            this.pnl1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl1;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblWinner;
+    }
+}
+
---
+++ Forms/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace guessing_game
+{
+    public partial class FGuessingGame : Form
+    {
+        static Random random = new Random();
+        int randomNumber;
+        int attempts;
+        bool winner = false;
+
+        public FGuessingGame()
+        {
+            InitializeComponent();
+        }
+
+        private void FGuessingGame_Load(object sender, EventArgs e)
+        {
+            randomNumber = random.Next(1, 101);
+            lblResult.ForeColor = Color.White;
+            lblWinner.ForeColor = Color.White;
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            if (winner == false)
+            {
+                if (int.TryParse(txtGuess.Text, out int guess))
+                {
+                    attempts++;
+
+                    if (guess > randomNumber)
+                    {
+                        lblResult.Text = $"Nice try! The number is below your guess";
+                        lblResult.ForeColor = Color.Black;
+                    }
+                    else if (guess < randomNumber)
+                    {
+                        lblResult.Text = $"Nice try! The number is above your guess";
+                        lblResult.ForeColor = Color.Black;
+                    }
+                    else
+                    {
+                        lblWinner.Text = $"Congratulations! You guessed the number in {attempts} attempts!";
+                        lblWinner.ForeColor = Color.Black;
+                        lblResult.ForeColor = Color.White;
+
+                        winner = true;
+                        btnGuess.Enabled = false;
+                    }
+                }
+                else
+                {
+                    lblResult.Text = "Please enter a valid number!";
+                    lblResult.ForeColor = Color.Red;
+                }
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "Game reset! Try to guess the new number.";
+            lblResult.ForeColor = Color.Black;
+            lblWinner.Text = string.Empty;
+            txtGuess.Clear();
+            attempts = 0;
+            randomNumber = random.Next(1, 101);
+            winner = false;
+            btnGuess.Enabled = true;
+        }
+
+        private void txtGuess_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
